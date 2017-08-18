0043a5cd1cda969df39f6a2a78a3786ab7caa4d6
chgrp-hung <hung@thechgrp.com>
2017-08-18 03:05:43 -0700

    cleanup

--- src/index.js
+++ src/index.js
@@ -10,9 +10,6 @@ import {Provider} from 'react-redux';
 
 
 const actions = bindActionCreators({updateCurrent}, store.dispatch);
-// const render = () => {
-//   //Store
-//   const state = store.getState();
 
 ReactDOM.render(
   <Provider store={store}>
@@ -20,35 +17,4 @@ ReactDOM.render(
   </Provider>,
 document.getElementById('root'));
 
-  // To change by wrapping our app with the Provider from react-redux
-  // ReactDOM.render(<App todos={state.todos}
-  //     currentTodo={state.currentTodo}
-  //     // changeCurrent = {actions.todoChangeHandler}  -- replace this with the shorthand key in l:24 below
-  //     changeCurrent = {actions.updateCurrent}
-  //   />,
-  //   document.getElementById('root'));
-// }
-
-// const todoChangeHandler = (value) => store.dispatch(updateCurrent(value));
-
-
-//State
-// const state = {
-//   todos: [
-//     {id: 1, name: 'Render static UI', isComplete: true},
-//     {id: 2, name: 'Create initial state', isComplete: true},
-//     {id: 3, name: 'Render based on state', isComplete: false},
-//   ]
-// };
-
-
-// To be replaced with the Provider (react-redux)
-// render();  //initial render
-// store.subscribe(render);
-
-
-// temporary to test subscribe
-// setTimeout(() => {
-//   store.dispatch({type: 'TODO_ADD', payload: {id: 5, name: "New todo", isComplete: false}});
-// }, 1000)
 registerServiceWorker();
