ec99f3bcd45c3c694c176ae2cf24b2aa50b3c717
chgrp-hung <hung@thechgrp.com>
2017-08-24 01:39:13 -0700

    16 dispatch async actions with redux thunk middleware

--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,17 +1,28 @@
 
+import {getTodos} from '../lib/todoService';
+
 const initState = {
-  todos: [
-    {id: 1, name: 'Render static UI', isComplete: true},
-    {id: 2, name: 'Create initial state', isComplete: true},
-    {id: 3, name: 'Render based on state', isComplete: false},
-  ],
+  todos: [],
+    // {id: 1, name: 'Render static UI', isComplete: true},
+    // {id: 2, name: 'Create initial state', isComplete: true},
+    // {id: 3, name: 'Render based on state', isComplete: false},
   currentTodo: ''
 }
 
 const CURRENT_UPDATE = 'CURRENT_UPDATE';
 const TODO_ADD = 'TODO_ADD';
+const TODO_LOAD = 'TODO_LOAD';
 
 export const updateCurrent = value => ({type: CURRENT_UPDATE, payload: value});
+
+export const loadTodos = (todos) => ({type: TODO_LOAD, payload: todos});
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos()
+      // .then(todos => console.log(todos));
+      .then(todos => dispatch(loadTodos(todos)));
+  }
+}
 
 export default (state = initState, action) => {
   switch (action.type) {
@@ -19,6 +30,8 @@ export default (state = initState, action) => {
       return {...state, todos: state.todos.concat(action.payload)};
     case CURRENT_UPDATE:
       return {...state, currentTodo:action.payload};
+    case TODO_LOAD:
+      return {...state, todos: action.payload};
     default:
       return state;
   }
