b81add7ecae2de968789db7c68b2167cc344f802
chgrp-hung <hung@thechgrp.com>
2017-08-25 04:19:30 -0700

    25 Filter State Data in Redux with a selector function and react router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import TodoForm from './components/TodoForm';
 import TodoList from './components/TodoList';
 import Message from './components/Message';
 import {connect} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Footer from './components/Footer';
 
 class App extends Component {
@@ -23,7 +23,9 @@ class App extends Component {
           <div className="Todo-App">
             <Message message="" />
             <TodoForm />
-            <TodoList />
+            <Route path ='/:filter?'render={({match}) => (
+              <TodoList filter={match.params.filter} />
+            )} />
             <Footer />
           </div>
         </Router>
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import {fetchTodos, toggleTodo, deleteTodo} from '../reducers/todo'
+import {fetchTodos, toggleTodo, deleteTodo, getVisibleTodos} from '../reducers/todo'
 
 const TodoItem = ({id, name, isComplete, toggleTodo, deleteTodo}) => (
   <li>
@@ -35,6 +35,7 @@ class TodoList extends Component {
 };
 
 export default connect(
-  (state) => ({todos: state.todo.todos}),
+  //mapStateToProps ownProps gives access to filter passed in by app.js but not yet accessible from TodoList component
+  (state, ownProps) => ({todos: getVisibleTodos(state.todo.todos, ownProps.filter)}),
   {fetchTodos, toggleTodo, deleteTodo}
 )(TodoList);
